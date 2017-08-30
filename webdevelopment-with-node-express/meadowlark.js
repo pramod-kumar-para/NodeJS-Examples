a5809452b50802f70297e2bd4c04ccb5dd2032c1
gs-ppara <parapramodkumar@gmail.com>
2017-08-30 21:59:01 +0530

    Handlebars with express basic example added

--- webdevelopment-with-node-express/meadowlark.js
+++ webdevelopment-with-node-express/meadowlark.js
@@ -1,39 +1,64 @@
 var express = require('express');
 var app = express();
-var handlebars = require('express4-handlebars').create({
+var handlebars = require('express3-handlebars').create({
     defaultLayout: 'main'
 });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 
+// express relies on a middle ware to handle static files and views.
+// The static middleware allows you to designate one or more directories
+// as containing static resources that are simply to be delivered to the 
+// client without any special handling.
+// has to be added before any routes.
+app.use(express.static(__dirname + '/public'));
+
 
 // In Express, the order in which routes and middleware are added is significant.
 // Express will also take care of stripping query string from URL.
 app.get('/', function (req, res) {
-    res.type('text/plain');
-    res.send('Meadowlark Travel');
+    // Below are used to send plain response 
+    // res.type('text/plain');
+    // res.send('Meadowlark Travel');
+
+    // sending hbs template
+    res.render('layouts/home');
 });
 
+// Examples of dynamic views.
+var fortunes = [
+    "Conquer your fears or they will conquer you.",
+    "Rivers need springs.",
+    "Do not fear what you don't know.",
+    "You will have a pleasant surprise.",
+    "Whenever possible, keep it simple.",
+];
 app.get('/about', function (req, res) {
-    res.type('text/plain');
-    res.send('About meadowlark Travel');
+    // res.type('text/plain');
+    // res.send('About meadowlark Travel');
+    var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+    res.render('layouts/about', {
+        fortune: randomFortune
+    });
 });
 
 // app.use is the method by which express adds middleware
 // custom 404 page
 app.use(function (req, res) {
-    res.type('text/plain');
+    // res.type('text/plain');
     res.status(404);
-    res.send('404 Not found');
+    // res.send('404 Not found');
+    res.render('layouts/404');
 });
 
 // custom 500 page
 app.use(function (err, req, res, next) {
     console.error(err.stack);
-    res.type('text/plain');
+    // res.type('text/plain');
     res.status(500);
-    res.send('500 Internal server');
+    // res.send('500 Internal server');
+    res.render('layouts/500');
 });
 
 app.listen(app.get('port'), function () {
